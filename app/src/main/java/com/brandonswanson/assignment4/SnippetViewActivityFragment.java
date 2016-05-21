f56678ffc789183705f590c41cc4de7d9dabd06f
Brandon Swanson <swanyriver@gmail.com>
2016-05-20 17:30:03 -0700

    serializable

--- app/src/main/java/com/brandonswanson/assignment4/SnippetViewActivityFragment.java
+++ app/src/main/java/com/brandonswanson/assignment4/SnippetViewActivityFragment.java
@@ -24,6 +24,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import android.os.Handler;
 
@@ -240,7 +247,7 @@ public class SnippetViewActivityFragment extends Fragment {
         }
     }
 
-private static class Snippet {
+private static class Snippet implements Serializable{
     public final String videoID;
     public final String url;
     public final String title;
@@ -294,8 +301,22 @@ private static class Snippet {
 }
 
     public Bundle getSerializedSnippet(){
+
+/*        ByteArrayOutputStream outString = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream objOut = new ObjectOutputStream(outString);
+            objOut.writeObject(mSnippets.get(mIndex));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
         Bundle output = new Bundle();
         Snippet snpt = mSnippets.get(mIndex);
+
+        /*output.putSerializable("snippet", snpt);
+        snpt = (Snippet) output.getSerializable("snippet");*/
+
         output.putString("VideoID", snpt.videoID);
         output.putString("url", snpt.url);
         output.putString("title", snpt.title);
