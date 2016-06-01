d387662511d94c17f75aba38e66c53bd31f4fa25
Brandon Swanson <swanyriver@gmail.com>
2016-05-31 22:40:25 -0700

    new icons and fab shell

--- app/src/main/java/com/brandonswanson/assignment4/MainActivity.java
+++ app/src/main/java/com/brandonswanson/assignment4/MainActivity.java
@@ -1,13 +1,16 @@
 package com.brandonswanson.assignment4;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //launch register log in/ log out activity
+
+                }
+            });
+        }
 
     }
 
